ee8a9b43b6fbc981ad4edf4516076451e700edaf
mark000777 <44491610+mark000777@users.noreply.github.com>
2019-03-08 19:31:38 +0800

    Update xuancai_ad.cc

--- xuancai_ad.cc
+++ xuancai_ad.cc
@@ -1,62 +1,82 @@
 [
-  {
-    "position": "splash_ad",
-    "max": 100,
-    "units": [
-      {
-        "platform": "tencent",
-        "ad_app_id": "1101152570",
-        "keywords": "",
-        "ad_unit_id": "8863364436303842593",
-        "quantity": 0
-      },
-      {
-        "platform": "baidu",
-        "ad_app_id": "e866cfb0",
-        "keywords": "",
-        "ad_unit_id": "2058622",
-        "quantity": 0
-      }
-    ]
-  },
-  {
-    "position": "call_show_ad",
-    "max": 100,
-    "units": [
-      {
-        "platform": "tencent",
-        "ad_app_id": "1101152570",
-        "keywords": "",
-        "ad_unit_id": "7030020348049331",
-        "quantity": 0
-      },
-      {
-        "platform": "baidu",
-        "ad_app_id": "e866cfb0",
-        "keywords": "",
-        "ad_unit_id": "2058628",
-        "quantity": 0
-      }
-    ]
-  },
-  {
-    "position": "banner_ad",
-    "max": 100,
-    "units": [
-      {
-        "platform": "tencent",
-        "ad_app_id": "1101152570",
-        "keywords": "",
-        "ad_unit_id": "9079537218417626401",
-        "quantity": 0
-      },
-      {
-        "platform": "baidu",
-        "ad_app_id": "e866cfb0",
-        "keywords": "",
-        "ad_unit_id": "2015351",
-        "quantity": 0
-      }
-    ]
-  }
+    {
+        "position":"splash_ad",
+        "max":100,
+        "units":[
+            {
+                "platform":"tencent",
+                "ad_app_id":"1101152570",
+                "keywords":"",
+                "ad_unit_id":"8863364436303842593",
+                "quantity":0
+            },
+            {
+                "platform":"baidu",
+                "ad_app_id":"e866cfb0",
+                "keywords":"",
+                "ad_unit_id":"2058622",
+                "quantity":0
+            }
+        ]
+    },
+    {
+        "position":"call_show_ad",
+        "max":100,
+        "units":[
+            {
+                "platform":"tencent",
+                "ad_app_id":"1101152570",
+                "keywords":"",
+                "ad_unit_id":"7030020348049331",
+                "quantity":0
+            },
+            {
+                "platform":"baidu",
+                "ad_app_id":"e866cfb0",
+                "keywords":"",
+                "ad_unit_id":"2058628",
+                "quantity":0
+            }
+        ]
+    },
+    {
+        "position":"banner_ad",
+        "max":100,
+        "units":[
+            {
+                "platform":"tencent",
+                "ad_app_id":"1101152570",
+                "keywords":"",
+                "ad_unit_id":"9079537218417626401",
+                "quantity":0
+            },
+            {
+                "platform":"baidu",
+                "ad_app_id":"e866cfb0",
+                "keywords":"",
+                "ad_unit_id":"2015351",
+                "quantity":0
+            }
+        ]
+    },
+    {
+        "position":"set_ok_ad",
+        "max":100,
+        "units":[
+            {
+                "platform":"tencent",
+                "ad_app_id":"1101152570",
+                "keywords":"",
+                "ad_unit_id":"7030020348049331",
+                "quantity":0
+            },
+            {
+                "platform":"baidu",
+                "ad_app_id":"e866cfb0",
+                "keywords":"",
+                "ad_unit_id":"2058628",
+                "quantity":0
+            }
+        ]
+    }
 ]
